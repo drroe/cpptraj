44815f47bd17a01c186c39c90e52b1e58955eae2
Daniel R. Roe <daniel.r.roe@gmail.com>
2017-07-28 16:17:04 -0400

    DRR - Cpptraj: Test of new base file class

---
+++ src/File.cpp
@@ -0,0 +1,296 @@
+#ifndef _WIN32
+#   include <wordexp.h>
+#endif
+#include <sys/stat.h> // stat
+#include <cstdio>     // FILE, fopen
+#include <cerrno>     // fileErrMsg, errno
+#include <cstring>    // fileErrMsg, strerror
+#include "File.h"
+#include "CpptrajStdio.h"
+
+#ifndef _WIN32
+static void WexpErr(int err) {
+  switch ( err ) {
+    case WRDE_BADCHAR :
+      mprinterr("Error: Illegal occurrence of newline or one of |, &, ;, <, >, (, ), {, }.\n");
+      break;
+    //case WRDE_BADVAL
+    case WRDE_CMDSUB :
+      mprinterr("Error: Command substitution is not allowed in file names.\n");
+      break;
+    case WRDE_NOSPACE :
+      mprinterr("Error: Out of memory.\n");
+      break;
+    case WRDE_SYNTAX :
+      mprinterr("Error: Bad syntax (unbalanced parentheses, unmatched quotes.\n");
+      break;
+  }
+}
+#endif /* _WIN32 */
+
+// ===== File::Name ============================================================
+// COPY CONSTRUCTOR
+File::Name::Name( const Name& rhs ) : fullPathName_(rhs.fullPathName_),
+  baseName_(rhs.baseName_), extension_(rhs.extension_),
+  compressExt_(rhs.compressExt_), dirPrefix_(rhs.dirPrefix_) {}
+
+// ASSIGNMENT
+File::Name& File::Name::operator=(const Name& rhs) {
+  if (this != &rhs) {
+    fullPathName_ = rhs.fullPathName_;
+    baseName_ = rhs.baseName_;
+    extension_ = rhs.extension_;
+    compressExt_ = rhs.compressExt_;
+    dirPrefix_ = rhs.dirPrefix_;
+  }
+  return *this;
+}
+
+// Name::clear()
+void File::Name::clear() {
+  fullPathName_.clear();
+  baseName_.clear();
+  extension_.clear();
+  compressExt_.clear();
+  dirPrefix_.clear();
+}
+
+bool File::Name::MatchFullOrBase(std::string const& rhs) const {
+  if (!fullPathName_.empty()) {
+    // Prefer full filename match.
+    if (fullPathName_ == rhs) return true;
+    if (baseName_     == rhs) return true;
+  }
+  return false;
+}
+
+int File::Name::SetName(std::string const& nameIn) {
+  // null filename allowed (indicates STDIN/STDOUT)
+  if (nameIn.empty()) {
+    clear();
+    return 0;
+  }
+#ifndef _WIN32
+  wordexp_t expanded;
+  int err = wordexp( nameIn.c_str(), &expanded, WRDE_NOCMD );
+  WexpErr( err );
+  if (err == 0) {
+    if (expanded.we_wordc < 1) { // Sanity check
+      mprinterr("Internal Error: Word expansion failed.\n");
+      err = 1;
+    } else
+      err = SetName_NoExpansion( expanded.we_wordv[0] );
+    wordfree( &expanded );
+  }
+  return err;
+#else
+  SetName_NoExpansion(nameIn);
+  return 0;
+#endif
+}
+
+int File::Name::SetName_NoExpansion(std::string const& nameIn) {
+  // null filename allowed (indicates STDIN/STDOUT)
+  if (nameIn.empty()) {
+    clear();
+    return 0;
+  }
+  // Assign filename with full path
+  fullPathName_.assign( nameIn );
+  // Get position of last occurence of '/' to determine base filename
+  size_t found = fullPathName_.find_last_of("/");
+  if (found == std::string::npos) {
+    baseName_ = fullPathName_;
+    dirPrefix_.clear();
+  } else {
+    baseName_ = fullPathName_.substr(found+1);
+    dirPrefix_ = fullPathName_.substr(0, found+1);
+  }
+  // Get the filename extension
+  found = baseName_.find_last_of(".");
+  if (found == std::string::npos) {
+    extension_.clear();
+  } else {
+    extension_ = baseName_.substr(found);
+  }
+  // See if the extension is one of the 2 recognized compression extensions.
+  // If file has a compression format extension, look for another extension.
+  if ( extension_ == ".gz" || extension_ == ".bz2" ) {
+    compressExt_ = extension_;
+    // Get everything before the compressed extension
+    std::string compressPrefix = baseName_.substr(0,found);
+    // See if there is another extension
+    found = compressPrefix.find_last_of(".");
+    if (found == std::string::npos)
+      // No other extension
+      extension_.clear();
+    else
+      extension_ = compressPrefix.substr(found);
+  } else
+    compressExt_.clear();
+  return 0;
+}
+
+int File::Name::Append( std::string const& suffix ) {
+  if (fullPathName_.empty()) return 1;
+  fullPathName_.append( suffix );
+  baseName_.append( suffix );
+  return 0;
+}
+
+File::Name File::Name::AppendName( std::string const& suffix ) const {
+  Name out( *this );
+  out.Append( suffix );
+  return out;
+}
+
+//TODO make this more efficient by just modifying full and base names
+File::Name File::Name::PrependName( std::string const& prefix ) const {
+  Name out;
+  out.SetName_NoExpansion(dirPrefix_ + prefix + baseName_);
+  return out;
+}
+
+File::Name File::Name::PrependExt( std::string const& extPrefix ) const {
+  Name out( *this );
+  // Find location of extension.
+  size_t found = out.baseName_.rfind( extension_ );
+  // Remove extension.
+  out.baseName_.resize( found );
+  // Insert extPrefix to just before extension and re-add extension.
+  out.baseName_.append( extPrefix + extension_ + compressExt_ );
+  // Update full path name.
+  out.fullPathName_ = dirPrefix_ + out.baseName_;
+  //mprintf("DEBUG: fullPathName= '%s'\n"
+  //        "       baseName=     '%s'\n"
+  //        "       extension=    '%s'\n"
+  //        "       compressExt=  '%s'\n"
+  //        "       dirPrefix=    '%s'\n",
+  //        out.fullPathName_.c_str(), out.baseName_.c_str(), out.extension_.c_str(),
+  //        out.compressExt_.c_str(), out.dirPrefix_.c_str());
+  return out;
+}
+
+// ===== File::Base ============================================================
+File::Base::Base() :
+  file_size_(0UL),
+  debug_(0),
+  isDos_(0),
+  access_(READ),
+  compressType_(NO_COMPRESSION),
+  isOpen_(false),
+  isStream_(false)
+{}
+
+int File::Base::ID_Type(const char* filenameIn) {
+  if (filenameIn == 0) return 1;
+  // Get basic file information
+  struct stat frame_stat;
+  if (stat(filenameIn, &frame_stat) == -1) {
+    mprinterr("Error: Could not find file status for %s\n", filenameIn);
+    if (debug_>0) 
+      perror("     Error from stat: ");
+    return 1;
+  }
+  file_size_ = frame_stat.st_size;
+  // Start off every file as a standard file
+  // ID by magic number - open for binary read access
+  FILE* fIn = fopen(filenameIn, "rb");
+  if ( fIn == 0 ) { 
+    mprinterr("Error: Could not open %s for hex signature read.\n", filenameIn);
+    return 1;
+  }
+  // Read first 3 bytes
+  unsigned char magic[3];
+  magic[0] = 0; 
+  magic[1] = 0; 
+  magic[2] = 0;
+  compressType_ = NO_COMPRESSION;
+  size_t numread = fread(magic, 1, 3, fIn);
+  if (numread == 0)
+    mprintf("Warning: File %s is empty\n", filenameIn);
+  else if (numread < 3 ) {
+    mprinterr("Warning: Could only read first %zu bytes of file %s.\n", numread, filenameIn);
+  } else {
+    if (debug_>0) mprintf("\t    Hex sig: %x %x %x", magic[0],magic[1],magic[2]);
+    // Check compression
+    if ((magic[0]==0x1f) && (magic[1]==0x8b) && (magic[2]==0x8)) {
+      if (debug_>0) mprintf(", Gzip file.\n");
+      compressType_ = GZIP;
+    } else if ((magic[0]==0x42) && (magic[1]==0x5a) && (magic[2]==0x68)) {
+      if (debug_>0) mprintf(", Bzip2 file.\n");
+      compressType_ = BZIP2;
+    } else if ((magic[0]==0x50) && (magic[1]==0x4b) && (magic[2]==0x3)) {
+      if (debug_>0) mprintf(", Zip file.\n");
+      compressType_ = ZIP;
+    } else {
+      if (debug_>0) mprintf(", No compression.\n");
+    }
+  }
+
+  // Additional file characteristics
+  // FIXME will not work for very large lines
+  char buffer[1024];
+  buffer[0]='\0';
+  if (fgets(buffer, 1023, fIn)==0) return 1;
+  fclose( fIn );
+
+  // Check for terminal CR before newline, indicates DOS file
+  size_t linesize = strlen(buffer);
+  if ( linesize > 1 ) {
+    if (buffer[ linesize - 2 ] == '\r') {
+      if (debug_>0) mprintf("  [DOS]");
+      isDos_ = 1;
+    }
+  }
+  return 0;
+}
+
+// =============================================================================
+File::NameArray File::ExpandToFilenames(std::string const& fnameArg) {
+  NameArray fnames;
+#ifdef _WIN32
+  fnames.push_back( fnameArg );
+#else
+  if (fnameArg.empty()) return fnames;
+  wordexp_t expanded;
+  int err = wordexp( fnameArg.c_str(), &expanded, WRDE_NOCMD );
+  WexpErr( err );
+  if ( err == 0 ) {
+    for (unsigned int i = 0; i != expanded.we_wordc; i++) {
+      if (expanded.we_wordv[i] == 0)
+        mprinterr("Internal Error: Bad expansion at %i\n", i);
+      else {
+        Name fn;
+        fn.SetName_NoExpansion( expanded.we_wordv[i] );
+        fnames.push_back( fn );
+      }
+    }
+    wordfree( &expanded );
+  }
+#endif /* _WIN32 */
+  return fnames;
+}
+
+static std::string fileErrMsg_ = std::string("");
+
+void File::ErrorMsg(const char* fname) {
+  mprinterr("Error: '%s': %s\n", fname, fileErrMsg_.c_str());
+}
+
+bool File::Exists(Name const& fn) {
+  if (!fn.empty()) {
+    FILE* infile = fopen(fn.full(), "rb");
+    if (infile==0) {
+      fileErrMsg_.assign( strerror(errno) );
+      return false;
+    }
+    fclose(infile);
+    return true;
+  }
+  return false;
+}
+
+bool File::Exists(std::string const& fname) {
+  return File::Exists( Name(fname) );
+}
---
+++ src/File.h
@@ -0,0 +1,85 @@
+#ifndef INC_FILE_H
+#define INC_FILE_H
+#include <string>
+#include <vector>
+/// This namespace contains useful file-related routines.
+namespace File {
+  /// File name, path, extension etc.
+  class Name;
+  /// Array of file names
+  typedef std::vector<Name> NameArray;
+  /// Expand given expression to array of file names
+  NameArray ExpandToFilenames(std::string const&);
+  /// File access types
+  enum AccessType { READ=0, WRITE, APPEND, UPDATE };
+  /// File compression types
+  enum CompressType { NO_COMPRESSION=0, GZIP, BZIP2, ZIP };
+  /// Basic file; no IO routines.
+  class Base;
+  /// Print error message corresponding to 'false' value from 'Exists()'
+  void ErrorMsg(const char*);
+  /// \return true if file exists and is accessible.
+  bool Exists(std::string const&); // TODO remove?
+  bool Exists(Name const&);
+}
+
+/** Class to hold file name, extension, etc. */
+class File::Name {
+  public:
+    Name() {}
+    Name(std::string const& s) { SetName(s); }
+    Name(const char* s) { SetName( std::string(s) ); }
+    Name(const Name&);
+    Name& operator=(const Name&);
+    /// Set file name and extensions, perform expansion as necessary.
+    int SetName(std::string const&);
+    /// Set file name, no expansions.
+    int SetName_NoExpansion(std::string const&);
+    /// Append given string to file name but do not change extension info.
+    int Append(std::string const&); //TODO this can probably replace AppendNumber some places
+    /// \return File name with given string appended - do not change extension info.
+    Name AppendName(std::string const&) const;
+    /// \return File name with given string prepended to base file name.
+    Name PrependName(std::string const&) const;
+    /// \return File name with given string prepended to extension
+    Name PrependExt(std::string const&) const;
+    /// Clear File name
+    void clear();
+    /// \return true if string matches full or base file name.
+    bool MatchFullOrBase(std::string const&) const;
+
+    const std::string& Full()      const { return fullPathName_;         }
+    const std::string& Base()      const { return baseName_;             }
+    const std::string& Ext()       const { return extension_;            }
+    const char* full()             const { return fullPathName_.c_str(); }
+    const char* base()             const { return baseName_.c_str();     }
+    const char* ext()              const { return extension_.c_str();    }
+    const std::string& Compress()  const { return compressExt_;          }
+    const std::string& DirPrefix() const { return dirPrefix_;            }
+    bool empty()                   const { return fullPathName_.empty(); }
+  private:
+    std::string fullPathName_;
+    std::string baseName_;
+    std::string extension_;
+    std::string compressExt_;
+    std::string dirPrefix_;
+};
+
+/** Base class that all files will inherit. */ 
+class File::Base {
+  public:
+    Base();
+    Name const& Filename() const { return fname_;  }
+    AccessType Access()    const { return access_; }
+    int ID_Type(const char*);
+  private:
+    Name fname_;
+    off_t file_size_;           ///< Actual file size
+    int debug_;
+    int isDos_;                 ///< 1 if CR present, need to count them as newlines
+    AccessType access_;         ///< Current file access
+    CompressType compressType_; ///< Type of compression present
+    bool isOpen_;               ///< True if file is open and ready for IO
+    bool isStream_;             ///< True if file is to/from a stream.
+};
+#endif
