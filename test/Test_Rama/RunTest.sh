5c09003234d9db2a74f0c1623cea25eacac642aa
Daniel R. Roe <daniel.r.roe@gmail.com>
2017-08-25 13:26:17 -0400

    DRR - Cpptraj: Update test

--- test/Test_Rama/RunTest.sh
+++ test/Test_Rama/RunTest.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles rama.in rama.dat total.dat dih.dat
+CleanFiles rama.in rama.dat total.dat dih.dat sum.agr
 
 INPUT='-i rama.in'
 
@@ -10,10 +10,12 @@ cat > rama.in <<EOF
 noprogress
 parm ../DPDP.parm7
 trajin ../DPDP.nc
-rama DPDP out rama.dat totalout total.dat :6-7
-multidihedral DIH phi psi resrange 6-7 out dih.dat
+#rama DPDP out rama.dat totalout total.dat :6-7
+rama usechars sumout sum.agr DPDP out rama.dat totalout total.dat
+multidihedral DIH phi psi resrange 6-7 out rama.dat
 EOF
 RunCpptraj "Ramachandran test."
+DoTest sum.agr.save sum.agr
 
 EndTest
 exit 0
