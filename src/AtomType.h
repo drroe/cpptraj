553a8de3362b43ed821d3926aceba5fd8c4fb6cc
Daniel R. Roe <daniel.roe@nih.gov>
2018-11-02 13:06:54 -0400

    DRR - Cpptraj: Fix up the equals and less-than operators

--- src/AtomType.h
+++ src/AtomType.h
@@ -7,16 +7,33 @@ class AtomType {
     AtomType() : mass_(0.0) {}
     /// Mass only
     AtomType(double m) : mass_(m) {}
-    /// Radius, well depth, mass TODO take LJparmType instead
+    /// Radius, well depth, mass
     AtomType(double r, double d, double m) : lj_(r, d), mass_(m) {}
     /// \return default LJ parameters
-    LJparmType const& LJ() const { return lj_; }
+    LJparmType const& LJ()   const { return lj_;   }
     /// \return Atom mass in amu
-    double Mass()          const { return mass_;   }
+    double Mass()            const { return mass_; }
+    /// \return True if this type has same parameters as incoming type
+    bool operator==(AtomType const& rhs) const {
+      return ( FEQ(mass_, rhs.mass_) &&
+               (lj_ == rhs.lj_) );
+    }
+    /// \return True if this type is at all different from incoming type
+    bool operator!=(AtomType const& rhs) const {
+      return ( FNE(mass_, rhs.mass_) ||
+               (lj_ != rhs.lj_) );
+    }
     /// \return true if LJ params are less than incoming
-    bool operator<(AtomType const& rhs) const { return lj_ < rhs.lj_; }
-    /// \return true if LJ params are the same
-    bool operator==(AtomType const& rhs) const { return lj_ == rhs.lj_; }
+    bool operator<(AtomType const& rhs) const {
+      if (*this != rhs) {
+        if (FEQ(mass_, rhs.mass_)) {
+          return lj_ < rhs.lj_;
+        } else {
+          return mass_ < rhs.mass_;
+        }
+      } else
+        return false;
+    }
     /// Used to modify LJ params
     LJparmType& SetLJ() { return lj_; }
   private:
