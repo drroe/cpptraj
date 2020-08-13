9875b635f0bc8a9b4eeffc41a515559259e923b0
Daniel R. Roe <daniel.r.roe@gmail.com>
2020-08-13 14:04:10 -0400

    Test the COORDS generation of replicatecell

--- test/Test_ReplicateCell/RunTest.sh
+++ test/Test_ReplicateCell/RunTest.sh
@@ -2,7 +2,7 @@
 
 . ../MasterTest.sh
 
-CleanFiles replicate.in cell.mol2 
+CleanFiles replicate.in cell.mol2 MyCoords.mol2
 
 TESTNAME='Replicate cell test'
 Requires netcdf maxthreads 1
@@ -13,10 +13,13 @@ cat > replicate.in <<EOF
 parm ../Test_SymmRmsd/TYR.parm7
 trajin ../Test_SymmRmsd/TYR.nc 1 1
 box x 20 y 20 z 20 beta 90
-replicatecell out cell.mol2 dir 100 dir 0-10 dir 001
+replicatecell out cell.mol2 dir 100 dir 0-10 dir 001 name MyCoords
+run
+crdout MyCoords MyCoords.mol2
 EOF
 RunCpptraj "$TESTNAME"
 DoTest cell.mol2.save cell.mol2
+DoTest cell.mol2.save MyCoords.mol2
 
 EndTest
 exit 0
