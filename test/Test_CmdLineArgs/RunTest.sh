2239c889b537b5752824af8719f7e343d6bba84b
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-02-04 11:31:32 -0500

    Test -ms option

--- test/Test_CmdLineArgs/RunTest.sh
+++ test/Test_CmdLineArgs/RunTest.sh
@@ -8,7 +8,17 @@
 # each test.
 TESTNAME='Command line arguments tests'
 
-CleanFiles out.crd test.traj.out test.tl.out
+CleanFiles out.crd test.traj.out test.tl.out test.ms.out
+
+# For old version of cpptraj that did not redirect output for command-line
+# arguments when -o given, this moves current test.out to target file.
+# TODO deprecate
+MoveTestOut() {
+  TGT=$1
+  if [ ! -f "$TGT" -a -f 'test.out' ] ; then
+    mv test.out $TGT
+  fi
+}
 
 # Test that -p, -y, and -x work. Also uses -o.
 #INPUT='-p ../tz2.parm7 -y ../tz2.crd -x out.crd -o test.traj.out'
@@ -17,10 +27,14 @@ CleanFiles out.crd test.traj.out test.tl.out
 # Test -tl
 INPUT='-p ../tz2.parm7 -y ../tz2.nc -y ../tz2.crd -y ../tz2.pdb -tl -o test.tl.out'
 RunCpptraj "Test command-line trajectory length"
-# For old version of cpptraj TODO deprecate
-if [ ! -f 'test.tl.out' -a -f 'test.out' ] ; then
-  mv test.out test.tl.out
-fi
+# TODO deprecate
+MoveTestOut test.tl.out
 DoTest test.tl.out.save test.tl.out
+
+# Test -ms
+INPUT='-p ../tz2.parm7 -ms @CA -o test.ms.out'
+RunCpptraj "Test command-line select atom numbers by mask"
+MoveTestOut test.ms.out
+DoTest test.ms.out.save test.ms.out
 
 EndTest
