5abcf3f25a0c7325363435849795e8495f5275ad
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-02-19 13:15:07 -0500

    Add code documentation.

--- src/CpptrajStdio.h
+++ src/CpptrajStdio.h
@@ -10,18 +10,33 @@
     thread, etc.
  */
 #ifdef MPI
+/// Set parallel world rank; will initially supress output for non-master ranks.
 void SetStdioRank(int);
 #endif
+/// Print to STDOUT
 void mprintf(const char *, ...);
+/// Print to STDERR
 void mprinterr(const char *, ...);
+/// Flush STDOUT
 void mflush();
+/// Print to STDOUT; prepend rank in MPI
 void rprintf(const char *, ...);
+/// Print to STDERR; prepend rank in MPI
 void rprinterr(const char *, ...);
+/// Can suppress/enable output.
 void SetWorldSilent(bool);
+/// Suppress all output/error forever. Use by pytraj.
 void SuppressAllOutput();
+/// Can suppress/enable error output.
 void SuppressErrorMsg(bool);
+/// Finalize any IO
 void FinalizeIO();
+/// Redirect output to given file.
 int OutputToFile(const char*);
+/// Redirect error output to given file.
 int ErrToFile(const char*);
+/// \retrun Current output FILE* stream as a void pointer.
 void* CpptrajStdout();
+/// \return Current error FILE* stream as a void pointer.
+void* CpprtajStderr();
 #endif
