afc333d3a856f53af4f3e3840f83163a34b79805
Daniel R. Roe <daniel.r.roe@gmail.com>
2021-11-10 11:06:12 -0500

    Set up fake trajectory list

--- unitTests/TrajFrameIndex/main.cpp
+++ unitTests/TrajFrameIndex/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <cmath>
+#include <string>
 #include <vector>
 #include "ArgList.h"
 //#include "Counter_Regular.h"
@@ -29,6 +30,7 @@ class Traj0 {
     }
 
     TrajFrameCounter const& Counter() const { return counter_; }
+    TrajFrameCounter& SetCounter() { return counter_; }
 
   private:
     TrajFrameCounter counter_;
@@ -40,22 +42,62 @@ class Fake {
     Fake() {}
 
     Traj0 const& Traj() const { return traj_; }
-    int FakeSetup(int nframes, ArgList& argIn) {
-      return traj_.Set(nframes, argIn);
+    int FakeSetup(std::string const& nameIn, int nframes, ArgList const& argIn) {
+      name_ = nameIn;
+      ArgList tmp = argIn;
+      return traj_.Set(nframes, tmp);
     }
-      
+
+    void Print() const {
+      traj_.Counter().PrintInfoLine(name_.c_str());
+    }
+
+    int BeginTraj() {
+      traj_.SetCounter().Begin();
+      return 0;
+    }
+
+    int GetNextFrame(int& frameIn) {
+      if (traj_.Counter().CheckFinished()) return 0;
+      frameIn = traj_.Counter().Current();
+      //if (ReadTrajFrame( traj_.Counter().Current(), frameIn )) return 0;
+      traj_.SetCounter().UpdateCounters();
+      return 1;
+    }
+
   private:
     Traj0 traj_;
+    std::string name_;
 };
 
+typedef std::vector<Fake*> ListType;
 
 int main() {
-  ArgList args("5 23 3");
+  ListType trajectories;
 
-  Fake fakeTraj;
-  if (fakeTraj.FakeSetup(100, args))
-    return Err("Could not set up fake trajectory.");
+  trajectories.push_back( new Fake() );
+  if (trajectories.back()->FakeSetup("trajectory1", 100, ArgList("5 22 3")))
+    return Err("Could not set up fake trajectory 1.");
+  trajectories.push_back( new Fake() );
+  if (trajectories.back()->FakeSetup("trajectory2", 20, ArgList("7 14 2")))
+    return Err("Could not set up fake trajectory 2.");
+  trajectories.push_back( new Fake() );
+  if (trajectories.back()->FakeSetup("trajectory3", 46, ArgList("42 46")))
+    return Err("Could not set up fake trajectory 3.");
 
+  int idx = 0;
+  for (ListType::const_iterator it = trajectories.begin(); it != trajectories.end(); ++it)
+  {
+    (*it)->Print();
+    (*it)->BeginTraj();
+    int fnum;
+    while ( (*it)->GetNextFrame(fnum) ) {
+      printf("[%8i] %8i\n", idx++, fnum+1);
+    }
+  }
+
+  for (ListType::iterator it = trajectories.begin(); it != trajectories.end(); ++it)
+    delete *it;
 
   return 0;
 }
