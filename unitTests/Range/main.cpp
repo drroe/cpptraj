99ac25020cb2316b059f5cacdb71c61c4008f282
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-01-11 12:47:22 -0500

    Improve Range unit test

--- unitTests/Range/main.cpp
+++ unitTests/Range/main.cpp
@@ -8,7 +8,20 @@ static const int Err(const char* msg) {
 }
 
 int main() {
-  Range range1;
+  Range range1("1-4,8,12,15-20");
+  Range::const_iterator it = range1.begin();
+  if (*(it++) != 1) return Err("Expected 1");
+  if (*(it++) != 2) return Err("Expected 2");
+  if (*(it++) != 3) return Err("Expected 3");
+  if (*(it++) != 4) return Err("Expected 4");
+  if (*(it++) != 8) return Err("Expected 8");
+  if (*(it++) != 12) return Err("Expected 12");
+  if (*(it++) != 15) return Err("Expected 15");
+  if (*(it++) != 16) return Err("Expected 16");
+  if (*(it++) != 17) return Err("Expected 17");
+  if (*(it++) != 18) return Err("Expected 18");
+  if (*(it++) != 19) return Err("Expected 19");
+  if (*(it++) != 20) return Err("Expected 20");
 
   return 0;
 }
