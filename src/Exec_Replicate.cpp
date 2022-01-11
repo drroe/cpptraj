0b0ec9b27bea925b14465e34e2858de415d331be
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-01-11 14:04:36 -0500

    Add frame keyword

--- src/Exec_Replicate.cpp
+++ src/Exec_Replicate.cpp
@@ -6,12 +6,12 @@
 void Exec_Replicate::Help() const
 {
   //mprintf("\t<mask> [nrep <#>] [%s]\n", DataSetList::TopIdxArgs);
-  mprintf("\tcrdset <coords set> [name <out coords>] [<mask>] [nrep <#>]\n");
+  mprintf("\tcrdset <coords set> [useframes <range>|frame <#>] [name <out coords>]\n"
+          "\t[<mask>] [nrep <#>]\n");
 }
 
 // Exec_Replicate::Execute()
-/** Replicate all or part of a system in a COORDS set. If there is more than
-  * 1 frame in the incoming COORDS set, assume we want that many replicates.
+/** Replicate all or part of a system in a COORDS set.
   */
 Exec::RetType Exec_Replicate::Execute(CpptrajState& State, ArgList& argIn)
 {
@@ -48,7 +48,7 @@ Exec::RetType Exec_Replicate::Execute(CpptrajState& State, ArgList& argIn)
       return CpptrajState::ERR;
     }
   } else {
-    useFrames = Range(0);
+    useFrames = Range( argIn.getKeyInt("frame", 1) - 1 );
     if (nrep == -1)
       nrep = 1;
   }
@@ -58,15 +58,7 @@ Exec::RetType Exec_Replicate::Execute(CpptrajState& State, ArgList& argIn)
   Frame frameIn = CRD->AllocateFrame();
   CRD->GetFrame(*(frmnum++), frameIn);
 
-/*  int tgtframe = argIn.getKeyInt("frame", 0);
-  if (tgtframe < 0 || tgtframe >= (int)CRD->Size()) {
-    mprinterr("Error: Specified frame %i is out of range.\n", tgtframe+1);
-    return CpptrajState::ERR;
-  }
-  Frame frameIn = CRD->AllocateFrame();
-  CRD->GetFrame(tgtframe, frameIn);
-  mprintf("    REPLICATE: Using COORDS '%s', frame %i\n", CRD->legend(), tgtframe+1);*/
-  // ----------
+  
   // Create output COORDS set if necessary
   DataSet_Coords* OUT = 0;
 //  int outframe = 0;
@@ -88,23 +80,12 @@ Exec::RetType Exec_Replicate::Execute(CpptrajState& State, ArgList& argIn)
     //OUT->AddFrame( CRD->AllocateFrame() );
     mprintf("\tOutput to set '%s'\n", OUT->legend());
   }
-  // ----------
   
+  AtomMask tempMask( argIn.GetMaskNext() );
+  // -------------------------
+
   // Topology from input coords will be modified.
   Topology topIn = CRD->Top();
-
-  /*Topology* parm = State.DSL().GetTopByIndex( argIn );
-  if (parm == 0) return CpptrajState::ERR;
-  // Check if this topology has already been used to set up an input
-  // trajectory, as this will break the traj read.
-  std::string fname = State.TopUsedInInputTraj(parm);
-  if (!fname.empty()) {
-    mprinterr("Error: Topology '%s' has already been used to set up trajectory '%s'.\n"
-              "Error:   To strip this topology use the 'strip' action.\n",
-              parm->c_str(), fname.c_str());
-    return CpptrajState::ERR;
-  }*/
-  AtomMask tempMask( argIn.GetMaskNext() );
 
   if (topIn.SetupIntegerMask( tempMask )) return CpptrajState::ERR;
   mprintf("\tReplicating atoms in topology '%s' %i times.\n", topIn.c_str(), nrep);
@@ -123,6 +104,9 @@ Exec::RetType Exec_Replicate::Execute(CpptrajState& State, ArgList& argIn)
     return CpptrajState::ERR;
   }
 
+  // Set
+
+  // Assign to output coords
   CoordinateInfo cinfo = CRD->CoordsInfo();
   if (OUT == 0) {
     // Want to replace set CRD with new top/coords.
