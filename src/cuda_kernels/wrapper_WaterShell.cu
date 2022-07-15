56291321afa25a14030832d3d599e5554dbd2857
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-07-15 14:26:29 -0400

    Fix array name

--- src/cuda_kernels/wrapper_WaterShell.cu
+++ src/cuda_kernels/wrapper_WaterShell.cu
@@ -27,16 +27,16 @@ static inline int Cuda_check(cudaError_t err, const char* desc) {
 }
 
 /** Calculate distances between pairs of atoms and bin them into a 1D histogram. */
-int Cpptraj_GPU_WaterShell(int& nlower, int& nupper,
+int Cpptraj_GPU_WaterShell(int* VatomShell,
                            CpptrajGpu::FpType lowerCut2, CpptrajGpu::FpType upperCut2,
                            const CpptrajGpu::FpType* xyz1, int N1,
                            const CpptrajGpu::FpType* xyz2, int N2,
                            ImageOption::Type imageType,
                            CpptrajGpu::HostBox<CpptrajGpu::FpType> const& box)
 {
-  int* device_counts;
-  Cuda_check(cudaMalloc(((void**)(&device_counts)), 2 * sizeof(int)), "Allocating watershell bins");
-  Cuda_check(cudaMemset( device_counts, 0, 2*sizeof(int) ), "Setting watershell bins to 0");
+  int* device_vatomshell;
+  Cuda_check(cudaMalloc(((void**)(&device_vatomshell)), N1 * sizeof(int)), "Allocating solvent atom shell array");
+  Cuda_check(cudaMemset( device_vatomshell, 0, N1*sizeof(int) ), "Setting solvent atom shells to 0");
 
   CpptrajGpu::FpType* device_xyz1;
   Cuda_check(cudaMalloc(((void**)(&device_xyz1)), N1 * 3 * sizeof(CpptrajGpu::FpType)), "Allocating xyz1");
@@ -62,37 +62,33 @@ int Cpptraj_GPU_WaterShell(int& nlower, int& nupper,
 
   // Launch kernel
   // Must have Non-overlapping coords
-  mprintf("DEBUG: before launch: nlower= %i nupper= %i\n", nlower, nupper);
   switch (imageType) {
       case ImageOption::NONORTHO:
         kWaterShell_nonOrtho<<<numBlocks, threadsPerBlock>>>(
-          device_xyz1, N1, device_xyz2, N2, gpuBox.FracDev(), gpuBox.UcellDev(), lowerCut2, upperCut2, device_counts);
+          device_xyz1, N1, device_xyz2, N2, gpuBox.FracDev(), gpuBox.UcellDev(), lowerCut2, upperCut2, device_vatomshell);
       break;
       case ImageOption::ORTHO:
         kWaterShell_Ortho<<<numBlocks, threadsPerBlock>>>(
-          device_xyz1, N1, device_xyz2, N2, gpuBox.BoxDev(), lowerCut2, upperCut2, device_counts);
+          device_xyz1, N1, device_xyz2, N2, gpuBox.BoxDev(), lowerCut2, upperCut2, device_vatomshell);
       break;
       case ImageOption::NO_IMAGE:
         kWaterShell_NoImage<<<numBlocks, threadsPerBlock>>>(
-          device_xyz1, N1, device_xyz2, N2, lowerCut2, upperCut2, device_counts);
+          device_xyz1, N1, device_xyz2, N2, lowerCut2, upperCut2, device_vatomshell);
       break;
       //default:
       //  mprinterr("Internal Error: kernel_rdf: Unhandled image type.\n");
       //  return 1;
   }
-  mprintf("DEBUG: after launch: nlower= %i nupper= %i\n", nlower, nupper);
 
   // Error check
   Cuda_check(cudaGetLastError(), "watershell kernel launch");
 
   // Copy the result back
-  int local_counts[2];
-  cudaMemcpy(local_counts, device_counts, 2*sizeof(int), cudaMemcpyDeviceToHost);
-  nlower = local_counts[0];
-  nupper = local_counts[1];
+  //int* local_counts[2];
+  cudaMemcpy(VatomShell, device_vatomshell, N1*sizeof(int), cudaMemcpyDeviceToHost);
 
   // Free device memory
-  cudaFree(device_counts);
+  cudaFree(device_vatomshell);
   cudaFree(device_xyz1);
   cudaFree(device_xyz2);
 
