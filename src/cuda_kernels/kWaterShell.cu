52b4e46f3d9a7ae942117c4e30167db10fc2bdc8
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-07-15 14:16:51 -0400

    Dont do the count on the GPU. Just set what shell each solvent atom is in

--- src/cuda_kernels/kWaterShell.cu
+++ src/cuda_kernels/kWaterShell.cu
@@ -6,7 +6,7 @@
 // -----------------------------------------------------------------------------
 /** Calculate # waters in 1st and 2nd solvation shells based on distance cutoffs. */
 __global__ void kWaterShell_NoImage(const CpptrajGpu::FpType* xyz1, int N1, const CpptrajGpu::FpType* xyz2, int N2,
-                                    CpptrajGpu::FpType lowerCut2, CpptrajGpu::FpType upperCut2, int* Counts)
+                                    CpptrajGpu::FpType lowerCut2, CpptrajGpu::FpType upperCut2, int* VatomShell)
 {
   int a1 = blockIdx.x * blockDim.x + threadIdx.x;
   int a2 = blockIdx.y * blockDim.y + threadIdx.y;
@@ -24,10 +24,10 @@ __global__ void kWaterShell_NoImage(const CpptrajGpu::FpType* xyz1, int N1, cons
 
     CpptrajGpu::FpType dist2 = (x*x) + (y*y) + (z*z);
     if (dist2 < upperCut2) {
-      atomicAdd( Counts + 1, 1 );
+      VatomShell[a1] = 2;
       if (dist2 < lowerCut2) {
         printf("a1= %i  a2= %i  dist= %f lower.\n", a1+1, a2+1, sqrt(dist2));
-        atomicAdd( Counts, 1 );
+        VatomShell[a1] = 1;
       }
     }
   }
@@ -36,7 +36,7 @@ __global__ void kWaterShell_NoImage(const CpptrajGpu::FpType* xyz1, int N1, cons
 /** Calculate # waters in 1st and 2nd solvation shells based on distance cutoffs. */
 __global__ void kWaterShell_Ortho(const CpptrajGpu::FpType* xyz1, int N1, const CpptrajGpu::FpType* xyz2, int N2,
                                   const CpptrajGpu::FpType* box,
-                                  CpptrajGpu::FpType lowerCut2, CpptrajGpu::FpType upperCut2, int* Counts)
+                                  CpptrajGpu::FpType lowerCut2, CpptrajGpu::FpType upperCut2, int* VatomShell)
 {
   int a1 = blockIdx.x * blockDim.x + threadIdx.x;
   int a2 = blockIdx.y * blockDim.y + threadIdx.y;
@@ -54,9 +54,9 @@ __global__ void kWaterShell_Ortho(const CpptrajGpu::FpType* xyz1, int N1, const 
 
     CpptrajGpu::FpType dist2 = ortho_dist2<CpptrajGpu::FpType>(a1x, a1y, a1z, a2x, a2y, a2z, box);
     if (dist2 < upperCut2) {
-      atomicAdd( Counts + 1, 1 );
+      VatomShell[a1] = 2;
       if (dist2 < lowerCut2)
-        atomicAdd( Counts, 1 );
+        VatomShell[a1] = 1;
     }
   }
 }
@@ -64,7 +64,7 @@ __global__ void kWaterShell_Ortho(const CpptrajGpu::FpType* xyz1, int N1, const 
 /** Calculate # waters in 1st and 2nd solvation shells based on distance cutoffs. */
 __global__ void kWaterShell_nonOrtho(const CpptrajGpu::FpType* xyz1, int N1, const CpptrajGpu::FpType* xyz2, int N2,
                                      const CpptrajGpu::FpType* frac, const CpptrajGpu::FpType* ucell,
-                                     CpptrajGpu::FpType lowerCut2, CpptrajGpu::FpType upperCut2, int* Counts)
+                                     CpptrajGpu::FpType lowerCut2, CpptrajGpu::FpType upperCut2, int* VatomShell)
 {
   int a1 = blockIdx.x * blockDim.x + threadIdx.x;
   int a2 = blockIdx.y * blockDim.y + threadIdx.y;
@@ -88,9 +88,9 @@ __global__ void kWaterShell_nonOrtho(const CpptrajGpu::FpType* xyz1, int N1, con
 
     CpptrajGpu::FpType dist2 =  NonOrtho_dist2<CpptrajGpu::FpType>(f2x, f2y, f2z, f1x ,f1y, f1z, ucell);
     if (dist2 < upperCut2) {
-      atomicAdd( Counts + 1, 1 );
+      VatomShell[a1] = 2;
       if (dist2 < lowerCut2)
-        atomicAdd( Counts, 1 );
+        VatomShell[a1] = 1;
     }
   }
 }
--- src/cuda_kernels/wrapper_WaterShell.cuh
+++ src/cuda_kernels/wrapper_WaterShell.cuh
@@ -2,7 +2,7 @@
 #define INC_WRAPPER_WATERSHELL_CUH
 #include "../Gpu.h"
 #include "../ImageOption.h"
-int Cpptraj_GPU_WaterShell(int&, int&, CpptrajGpu::FpType, CpptrajGpu::FpType,
+int Cpptraj_GPU_WaterShell(int*, CpptrajGpu::FpType, CpptrajGpu::FpType,
                            const CpptrajGpu::FpType*, int, const CpptrajGpu::FpType*, int,
                            ImageOption::Type,
                            CpptrajGpu::HostBox<CpptrajGpu::FpType> const&);
