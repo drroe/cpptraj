594ca1f4c3ed9287b28453965fb7679b39d7b210
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-11-05 15:49:30 -0400

    Add less than operator

--- src/Top/NbodyTerm.h
+++ src/Top/NbodyTerm.h
@@ -19,12 +19,26 @@ class NbodyTerm {
       idx_ = rhs.idx_;
       return *this;
     }
+
     /// \return Index into parameter set for this term.
     int Idx() const { return idx_; }
     /// Set index into parameter set for this term.
     void SetIdx(int idx) { idx_ = idx; }
     /// \return Atom # at specified position
     virtual int At(int idx) const = 0;
+    /// \return # atoms in this term
+    virtual int Nat() const = 0;
+    /// \return true if rhs is less than this
+    bool operator<(NbodyTerm const& rhs) {
+      if (Nat() == rhs.Nat()) {
+        for (int i = 0; i < Nat(); i++) {
+          if (At(i) != rhs.At(i)) {
+            return (At(i) < rhs.At(i));
+          }
+        }
+      }
+      return (Nat() < rhs.Nat());
+    }
   private:
     int idx_; ///< Index into corresponding parameter set.
 };
