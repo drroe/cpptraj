87cf3c3721650062547b7172e13e1616e0fb7fa2
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-11-05 15:58:42 -0400

    Clean up; add assignment and atom indices only constructor

--- src/Top/BondType.h
+++ src/Top/BondType.h
@@ -7,18 +7,28 @@ namespace Top {
 /// Hold bonded atom indices and parameter index
 class BondType : public NbodyTerm {
   public:
-    BondType() { at_[0]= 0; at_[1] = 0; }
+    /// CONSTRUCTOR
+    BondType() { at_[0]= -1; at_[1] = -1; }
+    /// CONSTRUCTOR - 2 atom indices
+    BondType(int a1, int a2) { at_[0] = a1; at_[1] = a2; }
+    /// CONSTRUCTOR - 2 atom indices, parameter index
     BondType(int a1, int a2, int idx) : NbodyTerm(idx) { at_[0] = a1; at_[1] = a2; }
+    /// COPY CONSTRUCTOR
     BondType(BondType const& rhs) : NbodyTerm(rhs) { at_[0] = rhs.at_[0]; at_[1] = rhs.at_[1]; }
-    inline int A1()  const { return at_[0];  }
-    inline int A2()  const { return at_[1];  }
+    /// ASSIGNMENT
+    BondType& operator=(BondType const& rhs) {
+      if (this == &rhs) return *this;
+      SetIdx(rhs.Idx());
+      at_[0] = rhs.at_[0];
+      at_[1] = rhs.at_[1];
+      return *this;
+    }
+
+    int A1()  const { return at_[0];  }
+    int A2()  const { return at_[1];  }
+
     int At(int idx) const { return at_[idx]; }
     int Nat() const { return 2; }
-/*    bool operator<(const BondType& rhs) const {
-      if (a1_ == rhs.a1_) {
-        return (a2_ < rhs.a2_);
-      } else return (a1_ < rhs.a1_);
-    }*/
   private:
     int at_[2];
 };
