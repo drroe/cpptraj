13768e1f034dd45876d5dece5d81f143303a1393
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-11-07 11:46:50 -0500

    Change to HookesLawType

--- src/Param/HookesLawType.h
+++ src/Param/HookesLawType.h
@@ -1,28 +1,37 @@
-#ifndef INC_PARAM_BONDPARMTYPE_H
-#define INC_PARAM_BONDPARMTYPE_H
+#ifndef INC_PARAM_HOOKESLAWTYPE_H
+#define INC_PARAM_HOOKESLAWTYPE_H
 #include <vector>
 #include "../FPcompare.h"
 namespace Cpptraj {
 namespace Param {
 using namespace FPcompare;
-/// Hold bond parameters
-class BondParmType {
+/// Hold parameters for a Hooke's Law term
+class HookesLawType {
   public:
-    BondParmType() : rk_(0), req_(0) {}
-    BondParmType(double rk, double req) : rk_(rk), req_(req) {}
-    inline double Rk()  const { return rk_;  }
-    inline double Req() const { return req_; }
-    inline void SetRk(double rk)   { rk_ = rk;   }
-    inline void SetReq(double req) { req_ = req; }
-    bool operator==(const BondParmType& rhs) const {
+    /// CONSTRUCTOR
+    HookesLawType() : rk_(0), req_(0) {}
+    /// CONSTRUCTOR - force constant, equilibrium value.
+    HookesLawType(double rk, double req) : rk_(rk), req_(req) {}
+    /// \return Force constant.
+    double Rk()  const { return rk_;  }
+    /// \return Equilibrium value (where force is zero).
+    double Req() const { return req_; }
+    /// Set the force constant.
+    void SetRk(double rk)   { rk_ = rk;   }
+    /// Set the equilibrium value.
+    void SetReq(double req) { req_ = req; }
+    /// \return True if given term is same as this one.
+    bool operator==(const HookesLawType& rhs) const {
       return ( FEQ<double>(rk_,  rhs.rk_ ) &&
                FEQ<double>(req_, rhs.req_) );
     }
-    bool operator!=(const BondParmType& rhs) const {
+    /// \return True if given term is different than this one.
+    bool operator!=(const HookesLawType& rhs) const {
       return ( FNE<double>(rk_,  rhs.rk_ ) ||
                FNE<double>(req_, rhs.req_) );
     }
-    bool operator<(const BondParmType& rhs) const {
+    /// \return True if this term is less than given term.
+    bool operator<(const HookesLawType& rhs) const {
       if (*this != rhs) {
         if (FEQ<double>(rk_, rhs.rk_)) {
           return (req_ < rhs.req_);
@@ -31,10 +40,11 @@ class BondParmType {
         return false;
     }
   private:
-    double rk_;
-    double req_;
+    double rk_;  ///< Force constant.
+    double req_; ///< Equilibrium value.
 };
-typedef std::vector<BondParmType> BondParmArray;
+/// Array of Hooke's Law parameters
+typedef std::vector<HookesLawType> HookesLawArray;
 }
 }
 #endif
