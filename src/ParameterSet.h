4e3067ddd0bff83d772c759f13f98f9c2de38ce0
Daniel R. Roe <daniel.r.roe@gmail.com>
2022-11-09 15:34:19 -0500

    Start modifying ParameterSet

--- src/ParameterSet.h
+++ src/ParameterSet.h
@@ -1,8 +1,10 @@
 #ifndef INC_PARAMETERSET_H
 #define INC_PARAMETERSET_H
-#include "ParameterTypes.h"
-#include "ParameterHolders.h"
-#include "AtomType.h"
+#include "Param/AtomType.h"
+#include "Param/ParmHolder.h"
+#include "Param/DihedralParmHolder.h"
+#include "Param/HookesLawType.h"
+using namespace Cpptraj::Param;
 /// Hold a set of parameters for atom types, bonds, angles, etc.
 class ParameterSet {
   public:
@@ -10,9 +12,9 @@ class ParameterSet {
 
     ParmHolder<AtomType>& AT()         { return atomTypes_; }
     ParmHolder<NonbondType>& NB()      { return nbParm_;    }
-    ParmHolder<BondParmType>& BP()     { return bondParm_; }
-    ParmHolder<AngleParmType>& AP()    { return angleParm_; }
-    ParmHolder<BondParmType>& UB()     { return ubParm_; }
+    ParmHolder<HookesLawType>& BP()     { return bondParm_; }
+    ParmHolder<HookesLawType>& AP()    { return angleParm_; }
+    ParmHolder<HookesLawType>& UB()     { return ubParm_; }
     //ParmHolder<DihedralParmType>& DP() { return dihParm_; }
     ParmHolder<DihedralParmType>& IP() { return impParm_; }
     DihedralParmHolder& DP()           { return dihParm_; }
@@ -22,9 +24,9 @@ class ParameterSet {
 
     ParmHolder<AtomType> const& AT()         const { return atomTypes_; }
     ParmHolder<NonbondType> const& NB()      const { return nbParm_;    }
-    ParmHolder<BondParmType> const& BP()     const { return bondParm_; }
-    ParmHolder<AngleParmType> const& AP()    const { return angleParm_; }
-    ParmHolder<BondParmType> const& UB()     const { return ubParm_; }
+    ParmHolder<HookesLawType> const& BP()     const { return bondParm_; }
+    ParmHolder<HookesLawType> const& AP()    const { return angleParm_; }
+    ParmHolder<HookesLawType> const& UB()     const { return ubParm_; }
     //ParmHolder<DihedralParmType> const& DP() const { return dihParm_; }
     ParmHolder<DihedralParmType> const& IP() const { return impParm_; }
     DihedralParmHolder const& DP()           const { return dihParm_; }
@@ -55,9 +57,9 @@ class ParameterSet {
     //AtomTypeArray atomTypes_;
     ParmHolder<AtomType> atomTypes_;
     ParmHolder<NonbondType> nbParm_;
-    ParmHolder<BondParmType> bondParm_;
-    ParmHolder<AngleParmType> angleParm_;
-    ParmHolder<BondParmType> ubParm_;
+    ParmHolder<HookesLawType> bondParm_;
+    ParmHolder<HookesLawType> angleParm_;
+    ParmHolder<HookesLawType> ubParm_;
     //ParmHolder<DihedralParmType> dihParm_;
     ParmHolder<DihedralParmType> impParm_;
     DihedralParmHolder dihParm_;
