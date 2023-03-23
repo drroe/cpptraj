3e6bac6ed049350c40298696bcb504f8c35d1763
Daniel R. Roe <daniel.roe@nih.gov>
2023-03-23 11:05:40 -0400

    Start solvate test

--- test/Test_MEAD/RunTest.sh
+++ test/Test_MEAD/RunTest.sh
@@ -8,6 +8,7 @@ CleanFiles cpptraj.in potential.dat
 
 INPUT='-i cpptraj.in'
 
+UNITNAME='MEAD potential test'
 # The MEAD potential output for these coords is:
 # 0.00249097
 # 0.0203602
@@ -22,8 +23,19 @@ mead \
   potential epsin 1 epsext 80 \
     fpt 0.0,0.0,0.0 fpt 2.0,0.0,0.0
 EOF
-RunCpptraj "MEAD potential test"
-
+RunCpptraj "$UNITNAME"
 DoTest potential.dat.save potential.dat
+
+UNITNAME='MEAD solvate test'
+cat > cpptraj.in <<EOF
+parm sphere.pqr pqr
+loadcrd sphere.pqr name SPHERE
+mead \
+  ogm 41,1.0 \
+  ogm 41,0.25 \
+  verbose 2 \
+  solvate epsin 1
+EOF
+RunCpptraj "$UNITNAME"
 
 EndTest
