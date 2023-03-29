2ed8d44ec37d86bd8e3bd8c69881ffd1090fe342
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-03-29 09:14:36 -0400

    Add sites file load

--- src/Structure/TitrationData.cpp
+++ src/Structure/TitrationData.cpp
@@ -1,6 +1,8 @@
 #include "TitrationData.h"
 #include "TitratableSite.h"
 #include "../NameType.h"
+#include "../CpptrajStdio.h"
+#include "../BufferedLine.h"
 
 using namespace Cpptraj::Structure;
 
@@ -8,3 +10,47 @@ using namespace Cpptraj::Structure;
 TitrationData::TitrationData()
 {}
 
+/** Load titratable sites data.
+  * \param sitesFileName File name containing residue numbers and site names.
+  * \param sitesDirName Directory name containins site files.
+  */
+int TitrationData::LoadTitrationData(std::string const& sitesFileName,
+                                     std::string const& sitesDirName)
+{
+  if (sitesFileName.empty()) {
+    mprinterr("Internal Error: sitesFileName is empty.\n");
+    return 1;
+  }
+  BufferedLine sitesFile;
+  if (sitesFile.OpenFileRead(sitesFileName)) {
+    mprinterr("Error: Could not open sites file '%s'\n", sitesFileName.c_str());
+    return 1;
+  }
+  const char* ptr = sitesFile.Line();
+  while (ptr != 0) {
+    // Expected format: <res#> <siteName>
+    int ntokens = sitesFile.TokenizeLine(" ");
+    if (ntokens != 2) {
+      mprinterr("Error: Malformed line in sites file '%s': %s\n", sitesFileName.c_str(), ptr);
+      mprinterr("Error: Expected 2 tokens, got %i\n", ntokens);
+      return 1;
+    }
+    int rnum = atoi( sitesFile.NextToken() ) - 1;
+    std::string sname( sitesFile.NextToken() );
+    mprintf("DEBUG: Res#=%i  siteName=%s\n", rnum + 1, sname.c_str());
+    ResNameMap::iterator it = ResToSitename_.lower_bound( rnum );
+    if (it == ResToSitename_.end() || it->first != rnum) {
+      // rnum - 1
+      it = ResToSitename_.insert( it, ResNamePair(rnum, sname) );
+    } else {
+      mprinterr("Error: Residue number %i already has an entry.\n", rnum + 1);
+      return 1;
+    }
+    // See if there is data for this site yet.
+    // Next line
+    ptr = sitesFile.Line();
+  }
+  sitesFile.CloseFile();
+  return 0;
+}
+    
--- src/Structure/TitrationData.h
+++ src/Structure/TitrationData.h
@@ -1,6 +1,8 @@
 #ifndef INC_TITRATIONDATA_H
 #define INC_TITRATIONDATA_H
 #include <vector>
+#include <string>
+#include <map>
 namespace Cpptraj {
 namespace Structure {
 // Fwd declares
@@ -9,8 +11,16 @@ class TitratableSite;
 class TitrationData {
   public:
     TitrationData();
+    /// Load titration data from files
+    int LoadTitrationData(std::string const&, std::string const&);
   private:
-    std::vector<TitratableSite> Sites_; ///< Hold data for potential sites
+    typedef std::pair<int,std::string> ResNamePair;
+    typedef std::map<int,std::string> ResNameMap;
+    typedef std::pair<std::string,TitratableSite> NameSitePair;
+    typedef std::map<std::string,TitratableSite> NameSiteMap;
+
+    ResNameMap ResToSitename_;  ///< Map residue numbers to site names
+    NameSiteMap NameToSite_;    ///< Map site names to titratable site data
 };
 }
 }
