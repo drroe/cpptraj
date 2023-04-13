849db967f196213be398a2d4d69eb9a377838c41
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-04-13 14:50:30 -0400

    Start adding setup

--- src/Structure/Protonator.cpp
+++ src/Structure/Protonator.cpp
@@ -1,6 +1,28 @@
 #include "Protonator.h"
+#include "../ArgList.h"
+#include "../CpptrajState.h"
+#include "../Mead/MultiFlexResults.h"
 
 using namespace Cpptraj::Structure;
 /** CONSTRUCTOR */
-Protonator::Protonator()
+Protonator::Protonator() :
+  site_intrinsic_pKas_(0),
+  site_site_matrix_(0),
+  n_mc_steps_(0),
+  start_pH_(0),
+  stop_pH_(0),
+  pH_increment_(0),
+  min_wint_(0),
+  fract_toler_(0),
+  mcmode_(MC_FULL),
+  logfile_(0)
 {}
+
+/** Set up options */
+int Protonator::SetupProtonator(CpptrajState& State, ArgList& argIn,
+                                Cpptraj::Mead::MultiFlexResults const& results)
+{
+  site_intrinsic_pKas_ = results.PkIntSet();
+
+  return 0;
+}
--- src/Structure/Protonator.h
+++ src/Structure/Protonator.h
@@ -1,12 +1,33 @@
 #ifndef INC_STRUCTURE_PROTONATOR_H
 #define INC_STRUCTURE_PROTONATOR_H
+class ArgList;
+class DataSet;
+class CpptrajFile;
+class CpptrajState;
 namespace Cpptraj {
+namespace Mead {
+class MultiFlexResults;
+}
 namespace Structure {
 /// Class for assigning protonation states to a structure
 class Protonator {
   public:
     Protonator();
+    // Set up from arguments
+    int SetupProtonator(CpptrajState&, ArgList&, Cpptraj::Mead::MultiFlexResults const&);
   private:
+    DataSet* site_intrinsic_pKas_; ///< DataSet containing calculated intrinsic pKas for each site.
+    DataSet* site_site_matrix_;    ///< DataSet containing site-site interactions in e^2/ang
+    int n_mc_steps_;               ///< Number of monte carlo steps
+    double start_pH_;              ///< Starting pH
+    double stop_pH_;               ///< final pH
+    double pH_increment_;          ///< pH increment
+    double min_wint_;              ///< Min inter (pK units) for 2-site transitions
+    double fract_toler_;           ///< Tolerance for reduced sites
+    int iseed_;                    ///< RNG seed
+    enum MCModeType { MC_FULL = 0, MC_REDUCED, MC_CLUSTER };
+    MCModeType mcmode_;            ///< MC mode for computing protonation vs pH
+    CpptrajFile* logfile_;         ///< Output log file
 };
 }
 }
