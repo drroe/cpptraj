a8a79132dd880705972acaac34ed917e448bd018
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-04-14 08:57:19 -0400

    Finish options

--- src/Structure/Protonator.cpp
+++ src/Structure/Protonator.cpp
@@ -24,7 +24,38 @@ int Protonator::SetupProtonator(CpptrajState& State, ArgList& argIn,
                                 Cpptraj::Mead::MultiFlexResults const& results)
 {
   site_intrinsic_pKas_ = results.PkIntSet();
+  if (site_intrinsic_pKas_ == 0) {
+    mprinterr("Internal Error: Site intrinsic pKa data set is missing.\n");
+    return 1;
+  }
   site_site_matrix_ = results.SiteSiteMatrixSet();
+  if (site_site_matrix_ == 0) {
+    mprinterr("Internal Error: Site-site interaction matrix data set is missing.\n");
+    return 1;
+  }
+  n_mc_steps_ = argIn.getKeyInt("nmcsteps", 10000);
+  start_pH_ = argIn.getKeyDouble("startph", 5.0);
+  stop_pH_ = argIn.getKeyDouble("stopph", 10.0);
+  pH_increment_ = argIn.getKeyDouble("phincr", 0.5);
+  min_wint_ = argIn.getKeyDouble("minwint", 2.0);
+  fract_toler_ = argIn.getKeyDouble("fracttol", 0.000001);
+  std::string mcstr = argIn.GetStringKey("mcmode");
+  if (mcstr.empty())
+    mcmode_ = MC_REDUCED;
+  else {
+    if (mcstr == "full" || mcstr == "0") //  0 for backwards compat.
+      mcmode_ = MC_FULL;
+    else if (mcstr == "reduced" || mcstr == "1") // 1 for backwards compat.
+      mcmode_ = MC_REDUCED;
+    else { // CLUSTER not yet implemented
+      mprinterr("Error: Bad value for 'mcmode': %s\n", mcstr.c_str());
+      return 1;
+    }
+  }
+  logfile_ = State.DFL().AddCpptrajFile( argIn.GetStringKey("mclog"),
+                                         "MC log file",
+                                         DataFileList::TEXT,
+                                         true );
 
   return 0;
 }
@@ -32,4 +63,12 @@ int Protonator::SetupProtonator(CpptrajState& State, ArgList& argIn,
 /** Print options */
 void Protonator::PrintOptions() const {
   if (site_intrinsic_pKas_ != 0) mprintf("\tSite intrinsic pKa set: %s\n", site_intrinsic_pKas_->legend());
+  if (site_site_matrix_ != 0) mprintf("\tSite-site interaction matrix set: %s\n", site_site_matrix_->legend());
+  mprintf("\tCalculating titration curves from pH %g to %g in increments of %g\n",
+          start_pH_, stop_pH_, pH_increment_);
+  mprintf("\tCutoff for site-site interactions in pH units is %g\n", min_wint_);
+  mprintf("\tTolerance for reduced sites is %g\n", fract_toler_);
+  static const char* MCMODESTR[] = { "full", "reduced", "cluster" };
+  mprintf("\tMC mode: %s\n", MCMODESTR[mcmode_]);
+  if (logfile_ != 0) mprintf("\tLog output to '%s'\n", logfile_->Filename().full());
 }
