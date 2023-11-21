08aaf110eee6ccf1f061ee5cb8c55cb025c673c9
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-11-21 11:12:40 -0500

    Add frcmod read test save

--- test/Test_ReadAmberFF/RunTest.sh
+++ test/Test_ReadAmberFF/RunTest.sh
@@ -11,5 +11,6 @@ readdata frcmod.tip3pf as frcmod name PARM
 writedata tip3pf.parm.dat PARM
 EOF
 RunCpptraj "Test read Amber force modification file."
+DoTest tip3pf.parm.dat.save tip3pf.parm.dat
 
 EndTest
