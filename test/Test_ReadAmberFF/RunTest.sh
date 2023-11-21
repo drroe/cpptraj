fc22746d6c82301a92a624db925241955ce55e20
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-11-21 11:17:50 -0500

    Add read amber FF file

--- test/Test_ReadAmberFF/RunTest.sh
+++ test/Test_ReadAmberFF/RunTest.sh
@@ -2,15 +2,19 @@
 
 . ../MasterTest.sh
 
-CleanFiles cpptraj.in tip3pf.parm.dat
+CleanFiles cpptraj.in tip3pf.parm.dat toyrna.parm.dat
 
 INPUT='-i cpptraj.in'
 
 cat > cpptraj.in <<EOF
 readdata frcmod.tip3pf as frcmod name PARM
 writedata tip3pf.parm.dat PARM
+
+readdata toyrna.dat as amberff name TOYRNA
+writedata toyrna.parm.dat TOYRNA
 EOF
-RunCpptraj "Test read Amber force modification file."
+RunCpptraj "Test read Amber FF and force modification files."
 DoTest tip3pf.parm.dat.save tip3pf.parm.dat
+DoTest toyrna.parm.dat.save toyrna.parm.dat
 
 EndTest
