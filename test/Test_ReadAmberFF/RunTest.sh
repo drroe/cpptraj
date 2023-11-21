b59d31f50df9ae9058dda347631e326edf75b8e9
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-11-21 11:09:19 -0500

    Start adding frcmod read test

---
+++ test/Test_ReadAmberFF/RunTest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+. ../MasterTest.sh
+
+CleanFiles cpptraj.in tip3pf.parm.dat
+
+INPUT='-i cpptraj.in'
+
+cat > cpptraj.in <<EOF
+readdata frcmod.tip3pf as frcmod name PARM
+writedata tip3pf.parm.dat PARM
+EOF
+RunCpptraj "Test read Amber force modification file."
+
+EndTest
