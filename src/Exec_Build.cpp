0d2c6cca5f5abdd733867521b5b9fd18bd67aafb
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-12-05 11:33:08 -0500

    Get parameter set(s) to use

--- src/Exec_Build.cpp
+++ src/Exec_Build.cpp
@@ -1,11 +1,12 @@
 #include "Exec_Build.h"
 #include "CpptrajStdio.h"
+#include "DataSet_Parameters.h"
 #include "Structure/GenerateAngles.h"
 
 // Exec_Build::Help()
 void Exec_Build::Help() const
 {
-  mprintf("\tcrdset <COORDS set> [frame <#>]\n");
+  mprintf("\tcrdset <COORDS set> [frame <#>] [parmset <param set> ...]\n");
 }
 
 // Exec_Build::Execute()
@@ -35,11 +36,59 @@ Exec::RetType Exec_Build::Execute(CpptrajState& State, ArgList& argIn)
   // Get modifiable topology
   Topology& topIn = *(coords.TopPtr());
 
-  // Generate angles
+  // Fill in atoms with templates
+
+  // Generate angles/dihedrals
   if (Cpptraj::Structure::GenerateAngles(topIn)) {
     mprinterr("Error: Could not generate angles/dihedrals for '%s'\n", topIn.c_str());
     return CpptrajState::ERR;
   }
+
+  // Get parameter sets.
+  typedef std::vector<DataSet_Parameters*> Parray;
+  Parray ParamSets;
+  std::string parmset = argIn.GetStringKey("parmset");
+  if (parmset.empty()) {
+    mprintf("\tNo parameter set(s) specified with 'parmset'; using any loaded sets.\n");
+    // See if there are any parameter sets.
+    DataSetList sets = State.DSL().GetSetsOfType( "*", DataSet::PARAMETERS );
+    for (DataSetList::const_iterator it = sets.begin(); it != sets.end(); ++it)
+      ParamSets.push_back( (DataSet_Parameters*)(*it) );
+  } else {
+    while (!parmset.empty()) {
+      DataSetList sets = State.DSL().GetSetsOfType( parmset, DataSet::PARAMETERS );
+      if (sets.empty()) {
+        mprintf("Warning: No parameter sets corresponding to '%s'\n", parmset.c_str());
+      } else {
+        for (DataSetList::const_iterator it = sets.begin(); it != sets.end(); ++it)
+          ParamSets.push_back( (DataSet_Parameters*)(*it) );
+      }
+      parmset = argIn.GetStringKey("parmset");
+    }
+  }
+  if (ParamSets.empty()) {
+    mprinterr("Error: No parameter sets.\n");
+    return CpptrajState::ERR;
+  }
+  mprintf("\tParameter sets:\n");
+  for (Parray::const_iterator it = ParamSets.begin(); it != ParamSets.end(); ++it)
+    mprintf("\t  %s\n", (*it)->legend());
+
+  // Combine parameters if needed
+  DataSet_Parameters* mainParmSet = 0;
+  bool free_parmset_mem = false;
+  if (ParamSets.size() == 1)
+    mainParmSet = ParamSets.front();
+  else {
+    free_parmset_mem = true;
+    mainParmSet = new DataSet_Parameters();
+    mprintf("\tCombining parameter sets.\n");
+    ParameterSet::UpdateCount UC;
+    for (Parray::const_iterator it = ParamSets.begin(); it != ParamSets.end(); ++it)
+      mainParmSet->UpdateParamSet( *(*it), UC, State.Debug() );
+  }
+
+  if (free_parmset_mem && mainParmSet != 0) delete mainParmSet;
 
   return CpptrajState::OK;
 }
