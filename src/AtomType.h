e544085d06b47840d66b5a8bd3994ac3aa197e88
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-12-07 13:59:27 -0500

    Have the < and == operators take mass and polarizability into account

--- src/AtomType.h
+++ src/AtomType.h
@@ -22,10 +22,24 @@ class AtomType {
     double Polarizability() const { return polarizability_; }
     /// \return Original atom type index. Useful when checking for off-diagonal NB parameters.
     int OriginalIdx()       const { return oidx_; }
-    /// \return true if LJ params are less than incoming
-    bool operator<(AtomType const& rhs) const { return lj_ < rhs.lj_; }
-    /// \return true if LJ params are the same
-    bool operator==(AtomType const& rhs) const { return lj_ == rhs.lj_; }
+    /// \return true if mass, polarizability, or LJ params are less than incoming
+    bool operator<(AtomType const& rhs) const {
+      if (FEQ(mass_, rhs.mass_)) {
+        if (FEQ(polarizability_, rhs.polarizability_)) {
+          return lj_ < rhs.lj_;
+        } else {
+          return polarizability_ < rhs.polarizability_;
+        }
+      } else {
+        return mass_ < rhs.mass_;
+      }
+    }
+    /// \return true if mass, polarizability, and LJ params are the same
+    bool operator==(AtomType const& rhs) const {
+      return (FEQ(mass_, rhs.mass_) &&
+              FEQ(polarizability_, rhs.polarizability_) &&
+              lj_ == rhs.lj_);
+    }
     /// Used to modify LJ params
     LJparmType& SetLJ() { return lj_; }
     /// \return data size  (2 double for LJparmType)
