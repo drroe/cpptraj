50d1c4be21f8a95b1d01eb19dc5e489059a0c677
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-12-18 09:16:17 -0500

    Add atom hybridization to AtomType

--- src/AtomType.h
+++ src/AtomType.h
@@ -4,14 +4,16 @@
 /// Hold parameters for a unique atom type
 class AtomType {
   public:
+    /// Atom hybridization types
+    enum HybridizationType { SP = 0, SP2, SP3, UNKNOWN_HYBRIDIZATION };
     /// CONSTRUCTOR
-    AtomType() : mass_(0.0), polarizability_(0.0), oidx_(-1) {}
+    AtomType() : mass_(0.0), polarizability_(0.0), oidx_(-1), hybrid_(UNKNOWN_HYBRIDIZATION) {}
     /// CONSTRUCTOR - Mass only
-    AtomType(double m) : mass_(m), oidx_(-1) {}
+    AtomType(double m) : mass_(m), polarizability_(0.0), oidx_(-1), hybrid_(UNKNOWN_HYBRIDIZATION) {}
     /// CONSTRUCTOR - Mass, polarizability
-    AtomType(double m, double p) : mass_(m), polarizability_(p), oidx_(-1) {}
+    AtomType(double m, double p) : mass_(m), polarizability_(p), oidx_(-1), hybrid_(UNKNOWN_HYBRIDIZATION) {}
     /// CONSTRUCTOR - Radius, well depth, mass, polarizability
-    AtomType(double r, double d, double m, double p) : lj_(r, d), mass_(m), oidx_(-1) {}
+    AtomType(double r, double d, double m, double p) : lj_(r, d), mass_(m), oidx_(-1), hybrid_(UNKNOWN_HYBRIDIZATION) {}
     /// Set type index
     void SetTypeIdx(int i) { oidx_ = i; }
     /// \return default LJ parameters
@@ -22,6 +24,8 @@ class AtomType {
     double Polarizability() const { return polarizability_; }
     /// \return Original atom type index. Useful when checking for off-diagonal NB parameters.
     int OriginalIdx()       const { return oidx_; }
+    /// \return Atom hybridization
+    HybridizationType Hybridization() const { return hybrid_; }
     /// \return true if mass, polarizability, or LJ params are less than incoming
     bool operator<(AtomType const& rhs) const {
       if (FEQ(mass_, rhs.mass_)) {
@@ -49,5 +53,6 @@ class AtomType {
     double mass_;           ///< Mass in amu
     double polarizability_; ///< Atomic polarizability in Ang^3
     int oidx_;              ///< Original atom type index, for indexing nonbond parameters.
+    HybridizationType hybrid_; ///< Atom hybridization
 };
 #endif
