22bb0dddf1056781edec1b62fc44e67081db2dfc
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-12-31 09:22:54 -0500

    Start ImproperParmHolder unit test

---
+++ unitTests/ImproperParmHolder/main.cpp
@@ -0,0 +1,35 @@
+// Unit test for ImproperParmHolder class
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include "ParameterHolders.h"
+
+static const int Err(const char* msg) {
+  fprintf(stderr, "Error: %s\n", msg);
+  return 1;
+}
+
+int main() {
+  ImproperParmHolder IP;
+
+  TypeNameHolder ip1(4);
+  ip1.AddName("O");
+  ip1.AddName("HO");
+  ip1.AddName("CT");
+  ip1.AddName("N");
+  //ip1.SortImproperByAlpha("X");
+
+  TypeNameHolder ip2(4);
+  ip2.AddName("O");
+  ip2.AddName("X");
+  ip2.AddName("CT");
+  ip2.AddName("X");
+  //ip2.SortImproperByAlpha("X");
+  
+  //printf("ip1 %s %s %s %s\n", *ip1[0], *ip1[1], *ip1[2], *ip1[3]);
+  //printf("ip2 %s %s %s %s\n", *ip2[0], *ip2[1], *ip2[2], *ip2[3]);
+
+
+  return 0;
+}
