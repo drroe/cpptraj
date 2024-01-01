7bb9a58caae834ffc253a60d37e06ffb13d566ee
Daniel R. Roe <daniel.r.roe@gmail.com>
2024-01-01 09:43:37 -0500

    Add some more tests

--- unitTests/ImproperParmHolder/main.cpp
+++ unitTests/ImproperParmHolder/main.cpp
@@ -1,8 +1,5 @@
 // Unit test for ImproperParmHolder class
 #include <cstdio>
-#include <vector>
-#include <string>
-#include <algorithm>
 #include "ParameterHolders.h"
 
 static const int Err(const char* msg) {
@@ -11,9 +8,6 @@ static const int Err(const char* msg) {
 }
 
 int main() {
-  ImproperParmHolder IP;
-  IP.SetWildcard('X');
-
   TypeNameHolder ip0(4);
   ip0.AddName("N*");
   ip0.AddName("CX");
@@ -27,27 +21,39 @@ int main() {
   ip1.AddName("N");
   //ip1.SortImproperByAlpha("X");
 
+  TypeNameHolder ip1a(4);
+  ip1a.AddName("N");
+  ip1a.AddName("O");
+  ip1a.AddName("CT");
+  ip1a.AddName("HO");
+
+  ImproperParmHolder IP0;
+  ParameterHolders::RetType ret = IP0.AddParm( ip1, DihedralParmType( 3.0, 1.0, 0.0 ), false );
+  if (ret == ParameterHolders::ERR) return Err("Could not add improper parameter");
+  bool found;
+  DihedralParmArray impropers = IP0.FindParam( ip1a, found );
+  if (!found) return Err("Could not find improper parameter (no wildcards).");
+
   TypeNameHolder ip2(4);
   ip2.AddName("X");
   ip2.AddName("X");
   ip2.AddName("CT");
   ip2.AddName("O");
-  ParameterHolders::RetType ret = IP.AddParm( ip2, DihedralParmType( 2.0, 1.0, 3.14159/2.0 ), false );
+
+  ImproperParmHolder IP;
+  ret = IP.AddParm( ip2, DihedralParmType( 2.0, 1.0, 3.14159/2.0 ), false );
   if (ret == ParameterHolders::ERR) return Err("Could not add improper parameter");
   //ip2.SortImproperByAlpha("X");
   
-  //printf("ip1 %s %s %s %s\n", *ip1[0], *ip1[1], *ip1[2], *ip1[3]);
-  //printf("ip2 %s %s %s %s\n", *ip2[0], *ip2[1], *ip2[2], *ip2[3]);
+  impropers = IP.FindParam( ip1, found);
+  if (found) return Err("Improper parameter search before wildcard added failed.");
 
-  bool found;
-  DihedralParmArray impropers = IP.FindParam( ip1, found );
-  if (!found) {
-    return Err("Improper parameter search with wildcard match failed.");
-  }
+  IP.SetWildcard('X');
+  impropers = IP.FindParam( ip1, found );
+  if (!found) return Err("Improper parameter search with wildcard match failed.");
+
   impropers = IP.FindParam( ip0, found );
-  if (found) {
-    return Err("Improper parameter search found something when it should not have.");
-  }
+  if (found) return Err("Improper parameter search found something when it should not have.");
 
   return 0;
 }
