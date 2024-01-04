d66a9e76430d179ce83150eeb4a9ebdfbec034a4
Daniel R. Roe <daniel.r.roe@gmail.com>
2024-01-04 10:11:38 -0500

    Generate impropers is now in separate file. Update dependencies.

--- src/Structure/GenerateConnectivity.h
+++ src/Structure/GenerateConnectivity.h
@@ -1,16 +1,12 @@
 #ifndef INC_STRUCTURE_GENERATECONNECTIVITY_H
 #define INC_STRUCTURE_GENERATECONNECTIVITY_H
 class Topology;
-class AtomType;
-template <typename Type> class ParmHolder;
 namespace Cpptraj {
 namespace Structure {
 /// Generate angle/torsion arrays from bond arrays in a Topology
 int GenerateAngleTorsionArrays(Topology&);
 /// Generate bond/angle/torsion arrays from atom connectivity in a Topology
 int GenerateBondAngleTorsionArrays(Topology&);
-/// Generate impropers in a Topology
-int GenerateImpropers(Topology&, ParmHolder<AtomType> const&);
 }
 }
 #endif
