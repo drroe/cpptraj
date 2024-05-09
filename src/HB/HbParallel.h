2c17d62e7d58059693cb1b21f9a4a681572dd62a
Daniel R. Roe <daniel.r.roe@gmail.com>
2024-05-09 15:02:53 -0400

    Start adding the parallel routines.

--- src/HB/HbParallel.h
+++ src/HB/HbParallel.h
@@ -2,15 +2,22 @@
 #define INC_HB_HBPARALLEL_H
 #ifdef MPI
 #include "../Parallel.h"
+#include <vector>
 #endif
 namespace Cpptraj {
 namespace HB {
+class Hbond;
 /// Hold routines for syncing up HB data in parallel (MPI)
 class HbParallel {
   public:
     HbParallel();
   private:
 #   ifdef MPI
+    /// Determine number of hydrogen bonds on each rank
+    static std::vector<int> GetRankNhbonds( int, Parallel::Comm const& );
+    /// Flatten given Hbond into arrays
+    static void HbondToArray(std::vector<double>&, std::vector<int>&, Hbond const&);
+
     Parallel::Comm trajComm_; ///< Across-trajectory communicator
 #   endif
 };
