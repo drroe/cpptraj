dc5d29d510618d7d7baee25dac91bad22ddd24a8
Daniel R. Roe <daniel.r.roe@gmail.com>
2024-06-20 15:34:10 -0400

    Add to make

--- src/Structure/RingFinder.cpp
+++ src/Structure/RingFinder.cpp
@@ -1,5 +1,7 @@
 #include "RingFinder.h"
 #include "../AtomMask.h"
+#include "../CpptrajStdio.h"
+#include "../Topology.h"
 
 using namespace Cpptraj::Structure;
 
@@ -12,9 +14,45 @@ int RingFinder::InitRingFinder(ArgList& argIn) {
   return 0;
 }
 
+static inline void visitAtom( int at, int previousAt, int res, Topology const& topIn, std::vector<bool>& Visited )
+{
+  if (Visited[at]) {
+    mprintf("Already visited %s\n", topIn.AtomMaskName(at).c_str());
+  } else {
+    Visited[at] = true;
+    Atom const& currentAt = topIn[at];
+    for (Atom::bond_iterator bat = currentAt.bondbegin(); bat != currentAt.bondend(); ++bat)
+    {
+      if (*bat != previousAt && topIn[*bat].ResNum() == res) {
+        visitAtom( *bat, at, res, topIn, Visited );
+      }
+    }
+  }
+}
+
 /** Set up ring finder for topology. */
 int RingFinder::SetupRingFinder(Topology const& topIn) {
   rings_.clear();
+
+  std::vector<bool> Visited(topIn.Natom(), false); // TODO only have visited for residue atoms
+  // Do not look for rings that span residues.
+  for (int res = 0; res < topIn.Nres(); res++) {
+    Residue const& currentRes = topIn.Res(res);
+    int idx = 0;
+    for (int at = currentRes.FirstAtom(); at != currentRes.LastAtom(); at++, idx++)
+    {
+      mprintf("Starting at %s\n", topIn.AtomMaskName(at).c_str());
+      Visited.assign( topIn.Natom(), false );
+      Visited[at] = true;
+      Atom const& currentAt = topIn[at];
+      for (Atom::bond_iterator bat = currentAt.bondbegin(); bat != currentAt.bondend(); ++bat)
+      {
+        if (topIn[*bat].ResNum() == res) {
+          visitAtom( *bat, at, res, topIn, Visited );
+        }
+      }
+    }
+  }
 
   return 0;
 }
