85f6a8706e99a1ea9ef66a8947f712de5feec3e1
Daniel R. Roe <daniel.r.roe@gmail.com>
2024-06-21 11:21:18 -0400

    Add separate class for calculating least squares plane

---
+++ src/Structure/LeastSquaresPlane.h
@@ -0,0 +1,25 @@
+#ifndef INC_STRUCTURE_LEASTSQUARESPLANE_H
+#define INC_STRUCTURE_LEASTSQUARESPLANE_H
+#include <vector>
+#include "../Vec3.h"
+class Frame;
+class AtomMask;
+class Vec3;
+namespace Cpptraj {
+namespace Structure {
+/// Used to calculate the vector normal to a plane passing through a series of points
+class LeastSquaresPlane {
+  public:
+    LeastSquaresPlane();
+    void CalcLeastSquaresPlane(Frame const&, AtomMask const&);
+  private:
+    static double solve_cubic_eq(double,double,double,double);
+    static Vec3 leastSquaresPlane(unsigned int, const double*);
+
+    std::vector<double> vcorr_; ///< Will hold selected coordinates after centering.
+    Vec3 nxyz_;                 ///< Hold vector normal to the plane
+    Vec3 cxyz_;                 ///< Hold center (origin of vector normal to the plane)
+};
+}
+}
+#endif
