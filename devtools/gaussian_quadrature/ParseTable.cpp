c4c4494233b052593c03d829024b3316e4d74f90
Daniel R. Roe <daniel.roe@nih.gov>
2025-09-03 15:44:22 -0400

    Start converting the lines to arrays

--- devtools/gaussian_quadrature/ParseTable.cpp
+++ devtools/gaussian_quadrature/ParseTable.cpp
@@ -1,20 +1,44 @@
+#include "ArgList.h"
 #include "BufferedLine.h"
 #include <string>
+#include <vector>
 #include <cstdio>
 
-int main(int argc, char** argv) {
+//double convert_
+
+int string_to_array(std::vector<double>& array, std::string const& str)
+{
+  // String should have no whitespace
+  ArgList line;
+  line.SetList( str, "$[]=(,);" );
+  if (line.Nargs() < 3) {
+    // Probably blank
+    return 0;
+  }
+  if (line[1] == "2") // DEBUG
+    line.PrintDebug();
+  line.MarkArg(0);
+  int order = line.getNextInteger(-1);
+  if (order < 1) {
+    fprintf(stderr, "Error: invalid order: %s\n", str.c_str());
+    return -1;
+  }
+  line.MarkArg(2);
+  array.clear();
+  for (int n = 0; n < order; n++)
+    array.push_back( line.getNextDouble(-999) );
+  if (order == 2) {
+    printf("DEBUG: order %i\n", order);
+    for (std::vector<double>::const_iterator it = array.begin(); it != array.end(); ++it)
+      printf("\t%16.8f\n", *it);
+  }
+  
+  return order;
+}
+
+int read_file(std::string const& fname)
+{
   BufferedLine infile;
-  std::string fname;
-  for (int iarg = 1; iarg < argc; iarg++)
-  {
-    std::string arg( argv[iarg] );
-    if (!arg.empty())
-      fname = arg;
-  }
-  if (fname.empty()) {
-    fprintf(stderr,"No file specified.\n");
-    return 1;
-  }
   fprintf(stderr,"Parsing '%s'\n", fname.c_str());
 
   if (infile.OpenFileRead( fname )) {
@@ -42,6 +66,8 @@ int main(int argc, char** argv) {
           }
           if (*it == ';') {
             printf("Expression: '%s'\n", currentExpression.c_str());
+            std::vector<double> array;
+            string_to_array( array, currentExpression );
             currentExpression.clear();
             in_array = false;
           }
@@ -51,5 +77,22 @@ int main(int argc, char** argv) {
     ptr = infile.Line();
   }
   infile.CloseFile();
+  return 0;
+}
+
+int main(int argc, char** argv) {
+  std::string fname;
+  for (int iarg = 1; iarg < argc; iarg++)
+  {
+    std::string arg( argv[iarg] );
+    if (!arg.empty())
+      fname = arg;
+  }
+  if (fname.empty()) {
+    fprintf(stderr,"No file specified.\n");
+    return 1;
+  }
+  read_file(fname);
+
   return 0;
 }
