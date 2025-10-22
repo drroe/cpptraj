097d7af052c244b208b484a354ce95d130b07f58
Daniel R. Roe <daniel.r.roe@gmail.com>
2025-10-22 15:32:46 -0400

    Start a pdb residue map type

---
+++ src/DataSet_PdbResMap.h
@@ -0,0 +1,35 @@
+#ifndef INC_DATASET_PDBRESMAP_H
+#define INC_DATASET_PDBRESMAP_H
+#include "DataSet.h"
+#include <map>
+#include <vector>
+#include <string>
+#include "PdbResMapType.h"
+/// Hold LEaP PDB residue name map 
+class DataSet_PdbResMap : public DataSet {
+    typedef std::vector<std::string> Sarray;
+    typedef std::pair<NameType, Sarray> PairType;
+    typedef std::map<NameType, Sarray> MapType;
+  public:
+    DataSet_PdbResMap();
+    static DataSet* Alloc() { return (DataSet*)new DataSet_PdbResMap(); }
+    // ----- DataSet functions -------------------
+    size_t Size()                                    const { return 0; }
+    void Info()                                      const { return; }
+    int Allocate(SizeArray const&)                         { return 1; }
+    void Add(size_t, const void*)                          { return; }
+    void WriteBuffer(CpptrajFile&, SizeArray const&) const { return; }
+    int Append(DataSet*)                                   { return 1; }
+    size_t MemUsageInBytes()                         const { return 0; }
+#   ifdef MPI
+    int Sync(size_t, std::vector<int> const&, Parallel::Comm const&) { return 1; }
+#   endif
+    // -------------------------------------------
+    /// Add a PDB name to unit name mapping
+    int AddPdbResMap(Cpptraj::PdbResMapType const&);
+
+    void PrintPdbResMap() const;
+  private:
+    MapType pdbResMap_; ///< Map PDB residue name to array of unit names: begin, non-terminal, end
+};
+#endif
