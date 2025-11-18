c7c772024ce4313adb3740ad30dfae900f18399e
Daniel R. Roe <daniel.r.roe@gmail.com>
2025-11-18 16:02:39 -0500

    Start Frame unit test

---
+++ unitTests/Frame/main.cpp
@@ -0,0 +1,14 @@
+// Unit test for NameType class
+#include <cstdio>
+#include "Frame.h"
+
+static const int Err(const char* msg) {
+  fprintf(stderr, "Error: %s\n", msg);
+  return 1;
+}
+
+int main() {
+  Frame frm1;
+
+  return 0;
+}
