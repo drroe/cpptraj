78cc8686eefc1f806bab36702ee1fdcaf5913289
Daniel R. Roe <daniel.r.roe@gmail.com>
2025-11-18 08:51:54 -0500

    Start adding the vdw bounding box code

--- src/Structure/Solvate.cpp
+++ src/Structure/Solvate.cpp
@@ -1,9 +1,56 @@
 #include "Solvate.h"
+#include "../ArgList.h"
+#include "../CpptrajStdio.h"
+#include "../Frame.h"
+#include "../Topology.h"
+#include "../Parm/ParameterSet.h"
 
 using namespace Cpptraj::Structure;
 
 /** CONSTRUCTOR */
-Solvate::Solvate()
+Solvate::Solvate() :
+  debug_(0)
 {
+}
+
+/** Initialize arguments. */
+int Solvate::InitSolvate(ArgList& argIn, int debugIn) {
+  debug_ = debugIn;
+
+  return 0;
+}
+
+/** Create box, fill with solvent */
+int Solvate::SolvateBox(Topology& topOut, Frame& frameOut, Cpptraj::Parm::ParameterSet const& set0) {
+  using namespace Cpptraj::Parm;
+  // Sanity check
+  if (topOut.Natom() != frameOut.Natom()) {
+    mprinterr("Internal Error: Solvate::SolvateBox(): Topology %s #atoms %i != frame #atoms %i\n",
+              topOut.c_str(), topOut.Natom(), frameOut.Natom());
+    return 1;
+  }
+  // TODO Remove any existing box info?
+  //if (frameOut.BoxCrd.HasBox())
+
+  // Set vdw bounding box
+  double Xmin, Ymin, Zmin;
+  double Xmax, Ymax, Zmax;
+
+  for (int at = 0; at < topOut.Natom(); at++)
+  {
+    // Get radius
+    double atom_radius = 0.0;
+    bool has_vdw = false;
+    if (topOut[at].HasType()) {
+      ParmHolder<AtomType>::const_iterator it = set0.AT().GetParam( TypeNameHolder(topOut[at].Type()) );
+      if (it != set0.AT().end() && it->second.HasLJ()) {
+        atom_radius = it->second.LJ().Radius();
+        has_vdw = true;
+      }
+    }
+    mprintf("DEBUG: Atom %s has_vdw= %i VDW=%f\n", topOut.AtomMaskName(at).c_str(), (int)has_vdw, atom_radius);
+  }
+
+  return 0;
 }
 
--- src/Structure/Solvate.h
+++ src/Structure/Solvate.h
@@ -1,13 +1,24 @@
 #ifndef INC_SOLVATE_H
 #define INC_SOLVATE_H
+class ArgList;
+class Frame;
+class Topology;
 namespace Cpptraj {
+namespace Parm {
+class ParameterSet;
+}
 namespace Structure {
-/// Used to add solvent to a COORDS set.
+/// Used to add solvent to a frame/topology pair.
 class Solvate {
   public:
     /// CONSTRUCTOR
     Solvate();
+    /// Initialize
+    int InitSolvate(ArgList&, int);
+    /// Solvate with box
+    int SolvateBox(Topology&, Frame&, Cpptraj::Parm::ParameterSet const&); 
   private:
+    int debug_;
 };
 }
 }
