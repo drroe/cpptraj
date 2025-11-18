7acf0b52d5b6273c15e92382ac9df65ed569167c
Daniel R. Roe <daniel.r.roe@gmail.com>
2025-11-18 08:21:42 -0500

    Start solvate class

---
+++ src/Structure/Solvate.cpp
@@ -0,0 +1,9 @@
+#include "Solvate.h"
+
+using namespace Cpptraj::Structure;
+
+/** CONSTRUCTOR */
+Solvate::Solvate()
+{
+}
+
---
+++ src/Structure/Solvate.h
@@ -0,0 +1,14 @@
+#ifndef INC_SOLVATE_H
+#define INC_SOLVATE_H
+namespace Cpptraj {
+namespace Structure {
+/// Used to add solvent to a COORDS set.
+class Solvate {
+  public:
+    /// CONSTRUCTOR
+    Solvate();
+  private:
+};
+}
+}
+#endif
